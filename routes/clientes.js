55e135af3957b58a55ecce174cfa18cb47958e4d
unknown <manuelchaparro94@gmail.com>
2023-02-11 17:53:13 -0400

    ola

--- routes/clientes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const Ctrlindex	= require('../controllers/clientes.controllers');
-const router = Router();
-
-router.route('/agregarCliente')
-		.post(Ctrlindex.agregarCliente)
-
-module.exports = router;
